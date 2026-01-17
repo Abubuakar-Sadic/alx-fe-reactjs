9af7aadc1cbc22850436916e253a019d847a6690
kelvinkwakye300 <builtbyabubakar@gmail.com>
2026-01-17 03:01:55 +0000

    made some corrections

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,7 +1,16 @@
-import UserInfo from './UserInfo';
 
-function ProfilePage() {
-  return <UserInfo />;
+import React, { useContext } from 'react';
+import UserContext from '../UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
-export default ProfilePage;
+export default UserProfile;
