6abda8badf07e2c429e797ff0c8e05f71422e699
Abubakar Sadic <builtbyabubakar@gmail.com>
2026-02-22 01:13:11 +0000

    react form handling practice

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import RegistrationForm from "./components/RegistrationForm.jsx";
+import FormikForm from "./components/formikForm.jsx";
+
+
+function App() {
+    return <FormikForm />;
+}
+
+export default App;
---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formData.username || !formData.email || !formData.password) {
+      setError("All fields are required.");
+      return;
+    }
+
+    setError("");
+    console.log("Form Submitted:", formData);
+
+    // Simulate API call
+    alert("User Registered Successfully!");
+  };
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>User Registration</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label><br />
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label>Email</label><br />
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label>Password</label><br />
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
